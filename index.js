e44dbc2b85c7281bd46bb4829e63099c825af0ba
Vince <vlee9527@gmail.com>
2022-07-31 01:42:08 +0800

    cors fix

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const { Server } = require("socket.io");
 const { MongoClient } = require("mongodb");
 const { v4: uuidv4 } = require("uuid");
 const jwt = require("jsonwebtoken");
-const cors = require("cors");
 const bcrypt = require("bcrypt");
+
+import cors from "cors";
 
 require("dotenv").config;
 
@@ -19,12 +20,12 @@ app.use(cors());
 app.use(express.json());
 
 //socket io chat
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
 // let users = [];
 
